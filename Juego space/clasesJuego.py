e0b150968eb35d464dee1e7f5997b2622d0e7630
david970218 <davidperez19940218@gmail.com>
2020-04-24 15:54:26 -0500

    "segundo juego"

--- Juego space/clasesJuego.py
+++ Juego space/clasesJuego.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+#2500 1800
+
 ANCHO=1200
 ALTO=600
 NEGRO=[0,0,0]
@@ -38,33 +40,18 @@ class Creator:
 
 
 class JugadorCreator(Creator):
-    def factory_method(self ):
+    def factory_method(self, pos):
         image = pygame.Surface([ 50 , 50 ])
         color = BLANCO
-        pos = [(ANCHO / 2) , (ALTO - 50)]
         vel = (0 , 0)
         return Jugador(image , color , pos , vel)
 
-
-class EnemigoCreator(Creator):
-    def factory_method(self):
-        image = pygame.Surface([50 , 50])
-        color = VERDE
-        x=random.randrange(ANCHO - 50)
-        y=random.randrange(50 ,ALTO - 100, 70)
-        pos = (x , y)
-        vx =random.randrange(9) + 1
-        vel = ( vx , 0)
-        enemigo = Enemigo(image , color , pos , vel)
-        enemigo.set_tmp()
-        return enemigo
-
-
-class BalaCreator(Creator):
-    def factory_method(self, pos , vy , color):
-        image = pygame.Surface([5 , 15])
-        vel = (0 , vy)
-        return Bala(image , color , pos , vel)
+class BloqueCreator(Creator):
+    def factory_method(self, pos , tam):
+        image = pygame.Surface(tam)
+        color = ROJO
+        vel = (0 , 0)
+        return Bloque(image , color , pos , vel)
 
 
 class Jugador(sprite):
@@ -72,49 +59,23 @@ class Jugador(sprite):
     def mover(self , x):
         self.rect.x = x
 
-    def disparo(self):
-        x , y = self.get_size()
-        xpos , ypos = self.get_pos()
-        x /= 2
-        x += xpos
-        y = ypos
-        balaCreator = BalaCreator()
-        return balaCreator.factory_method((x,y) , -5 , AMARILLO)
 
-class Bala(sprite):
+class Bloque(sprite):
     pass
-
-class Enemigo(sprite):
-    def set_tmp(self):
-        self.tmp = random.randrange(60)
-
-    def disparo(self):
-        x , y = self.get_size()
-        xpos , ypos = self.get_pos()
-        x /= 2
-        x += xpos
-        y = ypos
-        balaCreator = BalaCreator()
-        return  balaCreator.factory_method((x,y) , 10 , ROJO)
-
 
 class Juego:
     def __init__(self):
         #Definicion de variables
         self.gameOver = False
         self.ventana=pygame.display.set_mode([ANCHO,ALTO])
+        self.bloques = pygame.sprite.Group()
         self.jugadores=pygame.sprite.Group()
-        self.rivales=pygame.sprite.Group()
         self.jugadorCreator = JugadorCreator()
-        self.enemigoCreator = EnemigoCreator()
-        self.balas = pygame.sprite.Group()
-        self.balasRivales = pygame.sprite.Group()
-        self.j= self.jugadorCreator.factory_method()
+        self.bloqueCreator = BloqueCreator()
+        self.bloque = self.bloqueCreator.factory_method((300,300) , (100 , 70))
+        self.bloques.add(self.bloque)
+        self.j= self.jugadorCreator.factory_method((ANCHO/2 , ALTO/2))
         self.jugadores.add(self.j)
-        n=1
-        for i in range(n):
-            r = self.enemigoCreator.factory_method()
-            self.rivales.add(r)
 
     def Jugar(self):
         reloj=pygame.time.Clock()
@@ -125,29 +86,28 @@ class Juego:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     fin=True
-                if event.type == pygame.MOUSEMOTION:
-                    self.j.mover(event.pos[0] - 25)
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                             pause = True
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    self.balas.add(self.j.disparo())
+                    if event.key == pygame.K_UP:
+                        self.j.vely = -5
+                        self.j.velx = 0
+                    if event.key == pygame.K_LEFT:
+                        self.j.velx  = -5
+                        self.j.vely = 0
+                    if event.key == pygame.K_RIGHT:
+                        self.j.velx  = 5
+                        self.j.vely = 0
+                    if event.key == pygame.K_DOWN:
+                        self.j.velx  = 0
+                        self.j.vely = 5
+
             #Refresco
-            self.tick(self.rivales)
-            self.disparo_rivales(self.rivales , self.balasRivales)
-            self.update(self.balas)
-            self.update(self.balasRivales)
-            self.update(self.rivales)
             self.ventana.fill(NEGRO)
             self.jugadores.draw(self.ventana)
-            self.balas.draw(self.ventana)
-            self.balasRivales.draw(self.ventana)
-            self.rivales.draw(self.ventana)
-            self.colisiones_balas(self.balas, self.rivales)
-            self.eliminar_balas(self.balas)
-            self.eliminar_balas_rivales(self.balasRivales)
-            self.disparo_a_jugador()
-            self.fin_de_juego()
+            self.bloques.draw(self.ventana)
+            self.colisiones()
+            self.update(self.jugadores)
             pygame.display.flip()
             reloj.tick(40)
             while pause:
@@ -158,59 +118,41 @@ class Juego:
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
                             pause = False
-            print self.j.vidas
 
-    def colisiones_balas(self , balas , enemigos):
-        ls_col = []
-        for bala in balas:
-            ls_col = pygame.sprite.spritecollide(bala , enemigos, True)
-            if len(ls_col) > 0:
-                balas.remove(bala)
-        return ls_col
 
-    def eliminar_balas(self , balas):
-        for bala in balas:
-            if bala.rect.y < 50:
-                balas.remove(bala)
-        return None
 
     def update(self , sprites):
         for sprite in sprites:
             if sprite.rect.x + sprite.velx > ANCHO - 50  or sprite.rect.x + sprite.velx < 0:
                 sprite.velx *= -1
+            if sprite.rect.y + sprite.vely > ALTO - 50  or sprite.rect.y + sprite.vely < 0:
+                sprite.vely *= -1
             sprite.rect.x += sprite.velx
             sprite.rect.y += sprite.vely
 
-
-
-
-    def disparo_rivales(self , enemigos , balas):
-        for enemigo in enemigos:
-            if enemigo.tmp < 0:
-                balas.add(enemigo.disparo())
-                enemigo.tmp = TIEMPO_DISPARO_ENEMIGO
-
-
-    def tick(self , enemigos):
-        for enemigo in enemigos:
-            enemigo.tmp -= 1
-
-
-    def eliminar_balas_rivales(self , balas):
-        for bala in balas:
-            if bala.rect.y > ALTO - 10:
-                balas.remove(bala)
-        return None
-
-    def disparo_a_jugador(self):
+    def colisiones(self):
         ls_col = []
-        for bala in self.balasRivales:
-            ls_col  += pygame.sprite.spritecollide(bala, self.jugadores , False)
-            if len(ls_col) > 0:
-                self.j.vidas -=1
-                self.balasRivales.remove(bala)
-        return ls_col
+        for bloque in self.bloques:
+            ls_col = pygame.sprite.spritecollide(self.j , self.bloques , False)
+            for b in ls_col:
+                print len(ls_col)
+                if self.j.vely < 0:
+                    if self.j.rect.top < b.rect.bottom:
+                        self.j.rect.top = b.rect.bottom
+                        self.j.vely = 0
+                elif self.j.vely > 0:
+                        if self.j.rect.bottom > b.rect.top:
+                            self.j.rect.bottom = b.rect.top
+                            self.j.vely = 0
 
-    def fin_de_juego(self):
-        if self.j.vidas == 0:
-            self.gameOver = True
+
+            ls_col = pygame.sprite.spritecollide(self.j , self.bloques , False)
+            for b in ls_col:
+                if self.j.velx < 0:
+                    if self.j.rect.left < b.rect.right:
+                        self.j.rect.left = b.rect.right
+                        self.j.velx = 0
+                elif self.j.velx > 0:
+                    if self.j.rect.right > b.rect.left:
+                        self.j.rect.right = b.rect.left
+                        self.j.velx = 0
